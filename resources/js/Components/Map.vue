3bdaf4cc1df54e48235c776a158453afadd5b20a
Pedro Gabriel <63373521+PedroGXV@users.noreply.github.com>
2023-09-30 12:37:30 -0300

    Implemented basic mock

---
+++ resources/js/Components/Map.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { Loader } from '@googlemaps/js-api-loader';
+
+const loader = new Loader({
+    apiKey: "AIzaSyCCn1owwNSFIioIKHwpWOI1RqFc3HYTK8Y",
+    version: "weekly",
+    libraries: ["places"]
+});
+
+const mapOptions = {
+    center: {
+        lat: 0,
+        lng: 0
+    },
+    zoom: 4
+};
+
+
+loader
+    .importLibrary('maps')
+    .then(({Map}) => {
+        let map = new Map(document.getElementById("map"), mapOptions);
+
+        loader
+            .importLibrary('marker')
+            .then(({AdvancedMarkerElement}) => {
+                const position = { lat: -25.344, lng: 131.031 };
+
+                new AdvancedMarkerElement({
+                    map: map,
+                    position: position,
+                    title: "Uluru",
+                });
+            });
+    });
+
+
+</script>
+
+<template>
+    <div class="bg-base-200 p-2 rounded-xl w-3/5">
+        <div id="map" style="height: 500px;" class="rounded-xl">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import Footer from "@/Layouts/Footer.vue";
+import Header from "@/Layouts/Header.vue";
+</script>
+
+<template>
+    <div
+        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-center bg-gray-100 dark:bg-gray-950 selection:bg-red-500 selection:text-white"
+    >
+        <Header/>
+
+        <div class="w-5/6 flex m-auto">
+            <slot/>
+        </div>
+
+        <Footer/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
