8dc8ba6f14cbb7a4e681ad20eb9d035b83561d25
Pedro Gabriel <63373521+PedroGXV@users.noreply.github.com>
2023-09-30 14:15:12 -0300

    Added leaflet.js

--- resources/js/Components/Map.vue
+++ resources/js/Components/Map.vue
@@ -1,45 +1,21 @@
 <script setup>
-import { Loader } from '@googlemaps/js-api-loader';
+import * as Leaflet from 'leaflet/dist/leaflet.js';
+import {onMounted} from "vue";
 
-const loader = new Loader({
-    apiKey: "AIzaSyCCn1owwNSFIioIKHwpWOI1RqFc3HYTK8Y",
-    version: "weekly",
-    libraries: ["places"]
+onMounted(() => {
+    let map = Leaflet.map('map').setView([51.505, -0.09], 13);
+
+    Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 });
-
-const mapOptions = {
-    center: {
-        lat: 0,
-        lng: 0
-    },
-    zoom: 4
-};
-
-
-loader
-    .importLibrary('maps')
-    .then(({Map}) => {
-        let map = new Map(document.getElementById("map"), mapOptions);
-
-        loader
-            .importLibrary('marker')
-            .then(({AdvancedMarkerElement}) => {
-                const position = { lat: -25.344, lng: 131.031 };
-
-                new AdvancedMarkerElement({
-                    map: map,
-                    position: position,
-                    title: "Uluru",
-                });
-            });
-    });
-
 
 </script>
 
 <template>
     <div class="bg-base-200 p-2 rounded-xl w-3/5">
-        <div id="map" style="height: 500px;" class="rounded-xl">
+        <div id="map" style="min-height: 100%">
         </div>
     </div>
 </template>
